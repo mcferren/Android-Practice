8885b56692111d5bfbd1b90944774c74a408bd3c
Benjamin McFerren <mcferren@seasidesyndication.com>
2013-06-27 17:31:40 -0500

    initial commit: twitter search client app

---
+++ FavoriteTwitterSearches/src/com/deitel/favoritetwittersearches/FavoriteTwitterSearches.java
@@ -0,0 +1,22 @@
+package com.deitel.favoritetwittersearches;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FavoriteTwitterSearches extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_favorite_twitter_searches);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.favorite_twitter_searches, menu);
+		return true;
+	}
+
+}
